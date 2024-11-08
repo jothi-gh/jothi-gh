6933cf1639f357bb6638487ff2e6a3f5c1adbbdd
Jothi R <jothijoh01@gmail.com>
2024-11-08 23:26:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import React from 'react';
+import TicTacToe from './Components/TicTacToe/Tictactoe';
+
+const App = () => {
+  return (
+    <div >
+      
+      <TicTacToe/>
+
+
+    </div>
+  );
+}
+
+export default App;
